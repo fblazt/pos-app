c2d3e8aa8123ded183487c9427d936023e8b8121
Firman <fblazt@outlook.com>
2020-04-15 16:31:35 +0700

    Add history recent order table

---
+++ src/components/modules/HistoryTable.vue
@@ -0,0 +1,40 @@
+<template>
+  <section class="container w-full p-5 flex flex-col rounded-lg shadow-2xl">
+    <header class="flex flex-row justify-between items-center">
+      <h1 class="text-4xl font-normal">Recent Order</h1>
+      <button class="px-3 flex justify-center items-center bg-gray-300 rounded-full">
+        Today <i class="fas fa-sort-down"></i>
+      </button>
+    </header>
+    <table class="text-center mt-3">
+      <thead>
+        <tr>
+          <th class="uppercase text-md font-medium">Invoices</th>
+          <th class="uppercase text-md font-medium">Cashier</th>
+          <th class="uppercase text-md font-medium">Date</th>
+          <th class="uppercase text-md font-medium">Orders</th>
+          <th class="uppercase text-md font-medium">Amount</th>
+        </tr>
+      </thead>
+      <tbody class="text-gray-500">
+        <tr v-for="item in 10" :key="item.id" class="border-t border-b">
+          <td class="py-2">#10928</td>
+          <td class="py-2">Cashier 1</td>
+          <td class="py-2">06 October 2019</td>
+          <td class="py-2">Ice Tea, Salad with Peanut Sauce</td>
+          <td class="py-2">Rp. 120.000</td>
+        </tr>
+      </tbody>
+    </table>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'HistoryTable',
+};
+</script>
+
+<style>
+
+</style>
--- src/views/History.vue
+++ src/views/History.vue
@@ -3,10 +3,11 @@
     <Navbar/>
     <div class="h-full flex flex-row">
       <Sidebar/>
-      <main class="h-full w-full m-6 flex flex-col">
+      <main class="h-full w-full m-6 flex flex-col justify-center items-center">
         <section class="h-full w-full flex flex-row flex-wrap justify-evenly">
           <HistoryCard v-for="item in 3" :key="item.id"/>
         </section>
+        <HistoryTable/>
       </main>
     </div>
   </div>
@@ -16,6 +17,7 @@
 import Navbar from '@/components/modules/Navbar.vue';
 import Sidebar from '@/components/modules/Sidebar.vue';
 import HistoryCard from '@/components/modules/HistoryCard.vue';
+import HistoryTable from '@/components/modules/HistoryTable.vue';
 
 export default {
   name: 'History',
@@ -23,6 +25,7 @@ export default {
     Navbar,
     Sidebar,
     HistoryCard,
+    HistoryTable,
   },
 };
 </script>
