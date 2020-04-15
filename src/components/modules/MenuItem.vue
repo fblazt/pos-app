19044e7946fc7019ac8eb7ee60441e15582f3dab
Firman <fblazt@outlook.com>
2020-04-16 03:06:35 +0700

    Lil update

--- src/components/modules/MenuItem.vue
+++ src/components/modules/MenuItem.vue
@@ -1,10 +1,12 @@
 <template>
-  <div class="h-64 w-56 m-5">
-    <img
-      src="https://media3.s-nbcnews.com/j/newscms/2019_33/2203981/171026-better-coffee-boost-se-329p_67dfb6820f7d3898b5486975903c2e51.fit-760w.jpg"
-      alt="Menu items"
-      class="rounded-xl"
-    />
+  <div class="h-48 w-56 m-5">
+    <div>
+      <img
+        src="https://media3.s-nbcnews.com/j/newscms/2019_33/2203981/171026-better-coffee-boost-se-329p_67dfb6820f7d3898b5486975903c2e51.fit-760w.jpg"
+        alt="Menu items"
+        class="object-cover rounded-t-xl"
+      />
+    </div>
     <div>
       <p class="font-semibold">Dalgona Coffee</p>
       <p class="font-bold">Rp. 10.000</p>
@@ -14,7 +16,7 @@
 
 <script>
 export default {
-  name: 'MenuCard',
+  name: 'MenuItem',
 };
 </script>
 
