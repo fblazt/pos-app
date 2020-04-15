1d78b6dc0cd4df5ca8b39c83497166cc6667370d
Firman <fblazt@outlook.com>
2020-04-16 03:04:05 +0700

    Import chart

--- src/views/History.vue
+++ src/views/History.vue
@@ -7,6 +7,17 @@
         <section class="h-full w-full flex flex-row flex-wrap justify-evenly">
           <HistoryCard v-for="item in 3" :key="item.id"/>
         </section>
+        <section class="container mt-5 w-full p-5 flex flex-col rounded-lg shadow-lg">
+          <header class="flex flex-row justify-between items-center">
+            <h1 class="text-4xl font-normal">Revenue</h1>
+            <button class="px-3 flex justify-center items-center bg-gray-300 rounded-full">
+              Month <i class="fas fa-sort-down"></i>
+            </button>
+          </header>
+          <main>
+            <HistoryChart/>
+          </main>
+        </section>
         <HistoryTable class="mt-5"/>
       </main>
     </div>
@@ -14,9 +25,12 @@
 </template>
 
 <script>
+import { line } from 'vue-chartjs';
+
 import Navbar from '@/components/modules/Navbar.vue';
 import Sidebar from '@/components/modules/Sidebar.vue';
 import HistoryCard from '@/components/modules/HistoryCard.vue';
+import HistoryChart from '@/components/modules/HistoryChart.vue';
 import HistoryTable from '@/components/modules/HistoryTable.vue';
 
 export default {
@@ -25,7 +39,13 @@ export default {
     Navbar,
     Sidebar,
     HistoryCard,
+    HistoryChart,
     HistoryTable,
+  },
+  extends: line,
+  props: ['chartdata', 'options'],
+  mounted() {
+    this.renderChart(this.chartdata, this.options);
   },
 };
 </script>
