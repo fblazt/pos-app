11b728548219bf56751ff5fec1de2f90b2eb832e
Firman <fblazt@outlook.com>
2020-04-20 03:50:28 +0700

    Add pdf print

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import TModal from 'vue-tailwind/src/components/TModal.vue';
 import Vuelidate from 'vuelidate';
+import VueHtmlToPaper from 'vue-html-to-paper';
 import App from './App.vue';
 import router from './router';
 import store from './store';
@@ -8,6 +9,7 @@ import '@/assets/css/tailwind.css';
 
 Vue.use(TModal);
 Vue.use(Vuelidate);
+Vue.use(VueHtmlToPaper);
 
 Vue.config.productionTip = false;
 
